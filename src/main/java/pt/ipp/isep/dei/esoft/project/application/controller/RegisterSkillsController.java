06ffab5092d401dd0a57d952e44b911d994f8014
VascoRAzevedo <vasco.rafael.azevedo@gmail.com>
2024-04-18 11:34:19 +0100

    US01(implementation): controller finished (only small corrections required), changes in the sequence diagram

--- src/main/java/pt/ipp/isep/dei/esoft/project/application/controller/RegisterSkillsController.java
+++
@@ -1,9 +0,0 @@
-package pt.ipp.isep.dei.esoft.project.application.controller;
-
-import pt.ipp.isep.dei.esoft.project.repository.SkillRepository;
-
-public class RegisterSkillsController {
-
-    private SkillRepository skillsRepository;
-
-}

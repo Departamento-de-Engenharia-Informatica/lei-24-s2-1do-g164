b2bce7418b42e908da3f5dd4d857f7386e41e803
leonor <1230977@isep.ipp.pt>
2024-04-20 19:56:14 +0100

    US04(docs):implementations (start)

---
+++ src/main/java/pt/ipp/isep/dei/esoft/project/application/controller/AssignSkillsController.java
@@ -0,0 +1,16 @@
+package pt.ipp.isep.dei.esoft.project.application.controller;
+
+import pt.ipp.isep.dei.esoft.project.repository.*;
+
+public class AssignSkillsController {
+    private SkillRepository skillRepository;
+    private CollaboratorRepository collaboratorRepository;
+    private AuthenticationRepository authenticationRepository;
+
+    public AssignSkillsController(){
+        getSkillRepository();
+    }
+
+}
+
+

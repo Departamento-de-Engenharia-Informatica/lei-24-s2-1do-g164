49aec45f46ced5cd90386379a2c11b1ea9486a28
Dinis Araújo <dinis.araujo.costa@gmail.com>
2024-04-27 15:23:42 +0100

    TESTS: Unit tests for Collaborator and Vehicle (we need to review vehicle checkup)

---
+++ src/test/java/pt/ipp/isep/dei/esoft/project/domain/VehicleTest.java
@@ -0,0 +1,51 @@
+package pt.ipp.isep.dei.esoft.project.domain;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import pt.ipp.isep.dei.esoft.project.repository.VehicleTypeRepository;
+
+import java.time.LocalDate;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class VehicleTest {
+
+    private static VehicleTypeRepository type = VehicleTypeRepository.LIGHT_VEHICLE;
+    private Vehicle vehicle;
+
+    @BeforeEach
+    public void setUp() {
+        vehicle = new Vehicle("Toyota", "Corolla", "ABC123", type, 1500.0, 1200.0, 10000, "01-01-2020", "01-01-2021", 5000);
+    }
+
+    @Test
+    public void testNeedsCheckup_NoPreviousCheckup() {
+        assertTrue(vehicle.needsCheckup());
+    }
+
+    @Test
+    public void testNeedsCheckup_WithPreviousCheckup() {
+        vehicle.addCheckup(LocalDate.of(2024, 6, 1), 12000);
+        assertFalse(vehicle.needsCheckup());
+    }
+
+    @Test
+    public void testAddCheckup() {
+        vehicle.addCheckup(LocalDate.of(2024, 6, 1), 120000);
+        assertEquals(120000, vehicle.getCurrentKm());
+        assertEquals(120000, vehicle.getLastCheckup().get().getCurrentKms());
+    }
+
+    @Test
+    public void testEquals_SameVehicleID() {
+        Vehicle anotherVehicle = new Vehicle("Toyota", "Camry", "ABC123", type, 1600.0, 1300.0, 12000, "01-01-2020", "01-01-2021", 5000);
+        assertTrue(vehicle.equals(anotherVehicle));
+    }
+
+    @Test
+    public void testEquals_DifferentVehicleID() {
+        Vehicle anotherVehicle = new Vehicle("Toyota", "Camry", "XYZ789", type, 1600.0, 1300.0, 12000, "01-01-2020", "01-01-2021", 5000);
+        assertFalse(vehicle.equals(anotherVehicle));
+    }
+
+}

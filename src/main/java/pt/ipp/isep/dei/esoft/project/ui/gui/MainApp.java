9d99479d093f529d27a25e32a0b330655e6668c9
VascoRafaelAzevedo <1230776@isep.ipp.pt>
2024-05-18 10:32:29 +0100

    Try to include JavaFX in the project

---
+++ src/main/java/pt/ipp/isep/dei/esoft/project/ui/gui/MainApp.java
@@ -0,0 +1,22 @@
+package pt.ipp.isep.dei.esoft.project.ui.gui;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class MainApp extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("src/main/resources/fxml/main.fxml"));
+        primaryStage.setTitle("JavaFX and Maven");
+        primaryStage.setScene(new Scene(root, 800, 600));
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}

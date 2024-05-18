4c3ea5d08986db796070d93b5341b31cdb9c7a97
Dinis Araújo <dinis.araujo.costa@gmail.com>
2024-05-18 11:37:19 +0100

    Global(implementation): Implementation of JavaFX into the project

---
+++ src/main/java/module-info.java
@@ -0,0 +1,30 @@
+module pt.ipp.isep.dei.esoft.project.ui.gui {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires AuthLib;
+    requires org.apache.commons.lang3;
+    requires java.logging;
+    requires org.jfree.jfreechart;
+    requires java.desktop;
+    requires reload4j;
+    requires guru.nidi.graphviz;
+
+//    requires commons.math3;
+
+    //opens fxml to javafx.fxml;
+    opens pt.ipp.isep.dei.esoft.project.application.controller.authorization to javafx.fxml;
+    opens pt.ipp.isep.dei.esoft.project.application.controller to javafx.fxml;
+    opens pt.ipp.isep.dei.esoft.project.application.session to javafx.fxml;
+    opens pt.ipp.isep.dei.esoft.project.domain to javafx.fxml;
+    //opens pt.ipp.isep.dei.esoft.project.DTO to javafx.fxml;
+    opens pt.ipp.isep.dei.esoft.project.repository to javafx.fxml;
+    opens pt.ipp.isep.dei.esoft.project.ui.gui to javafx.fxml;
+    opens pt.ipp.isep.dei.esoft.project.ui.console to javafx.fxml;
+    opens pt.ipp.isep.dei.esoft.project.ui.console.authorization to javafx.fxml;
+    opens pt.ipp.isep.dei.esoft.project.ui.console.menu to javafx.fxml;
+    opens pt.ipp.isep.dei.esoft.project.ui.console.utils to javafx.fxml;
+    opens pt.ipp.isep.dei.esoft.project.ui.gui.controllers to javafx.fxml;
+    exports pt.ipp.isep.dei.esoft.project.ui.gui;
+    exports pt.ipp.isep.dei.esoft.project.ui.gui.controllers;
+
+}
--- src/main/java/pt/ipp/isep/dei/esoft/project/ui/gui/MainApp.java
+++ src/main/java/pt/ipp/isep/dei/esoft/project/ui/gui/MainApp.java
@@ -6,12 +6,15 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.io.File;
+
 public class MainApp extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("src/main/resources/fxml/main.fxml"));
-        primaryStage.setTitle("JavaFX and Maven");
+        File file = new File("src\\main\\resources\\fxml\\main.fxml");
+        FXMLLoader loader = new FXMLLoader(file.toURL());
+        Parent root = loader.load();
         primaryStage.setScene(new Scene(root, 800, 600));
         primaryStage.show();
     }
